f3784d809c0e101b1ef634cbc5cfedc72b0efb19
Tuluhan <GalaFreakOfficial@gmail.com>
2026-02-23 23:39:58 +0100

    feat: changed variable types
    + establish new classes as reference variables
    + new properties for Case
    + added toString() functionality

--- src/main/java/de/ase/pcpartpicker/parts/Case.java
+++ src/main/java/de/ase/pcpartpicker/parts/Case.java
@@ -1,16 +1,31 @@
 package de.ase.pcpartpicker.parts;
+import de.ase.pcpartpicker.parts.additional.Form;
+import de.ase.pcpartpicker.parts.additional.Type;
 
+// Gehäuse eines Computers
 public class Case extends Part {
 
-    private String form;
+    private Form form; // Formfaktor, z.B. ATX, Micro-ATX, Mini-ITX
+    private boolean hasWindow; // Ob das Gehäuse ein Fenster hat oder nicht
+    private int fanSlots; // Anzahl der Slots für Gehäuselüfter (im ersten Schritt nur 120mm Lüfter)
     
-    public Case(float price, String manufacturer, String model, String form) {
+    public Case(double price, String manufacturer, String model, String form, boolean hasWindow, int fanSlots) {
         super(price, manufacturer, model);
-        this.form = form;
+        this.type = new Type("Case");
+        this.form = new Form(form);
+        this.hasWindow = hasWindow;
+        this.fanSlots = fanSlots;
     }
 
     public String getForm() {
-        return form;
+        return form.getForm();
     }
 
+    public boolean hasWindow() {
+        return hasWindow;
+    }
+
+    public int getfanSlots() {
+        return fanSlots;
+    }
 }
--- src/main/java/de/ase/pcpartpicker/parts/Part.java
+++ src/main/java/de/ase/pcpartpicker/parts/Part.java
@@ -1,25 +1,30 @@
 package de.ase.pcpartpicker.parts;
+import de.ase.pcpartpicker.parts.additional.Type;
 //import java.util.Date;
 
 public abstract class Part {
 
     // Grundlegende Eigenschaften, die jedes PC Teil besitzt
-    private float price;
-    private String type;
+    protected Type type;
+    private double price;
     private String manufacturer;
     private String model;
 
     // optional
     //private Date releasedate;
 
-    public Part(float price, String manufacturer, String model) {
+    public Part(double price, String manufacturer, String model) {
         this.price = price;
         this.manufacturer = manufacturer;
         this.model = model;
     }
 
     // Stand jetzt nicht gedacht, dass man die Eigenschaften ändern kann, deswegen nur Getter
-    public float getPrice() {
+    public String getType() {
+        return type.getType();
+    }
+
+    public double getPrice() {
         return price;
     }
 
@@ -29,5 +34,9 @@ public abstract class Part {
 
     public String getModel() {
         return model;
+    }
+
+    public String toString() {
+        return "Type: " + type.getType() + "\nPrice: " + price + "\nManufacturer: " + manufacturer + "\nModel: " + model + "\n";
     }
 }
