773a03ada76215ecb92024586e88e90c5d8b84cc
Tuluhan <GalaFreakOfficial@gmail.com>
2026-02-28 14:32:22 +0100

    feat: formfactors case
    now there are two different Forms, one for Motherboard and one for PSU

--- src/main/java/de/ase/pcpartpicker/parts/Case.java
+++ src/main/java/de/ase/pcpartpicker/parts/Case.java
@@ -5,20 +5,30 @@ import de.ase.pcpartpicker.parts.additional.Type;
 // Gehäuse eines Computers
 public class Case extends Part {
 
-    private Form form; // Formfaktor, z.B. ATX, Micro-ATX, Mini-ITX
+    private Form formMB; // Formfaktor Mainboard, z.B. ATX, Micro-ATX, Mini-ITX
+    private Form formPSU; // Formfaktor Netzteil, z.B. ATX, SFX, FlexATX, SFX-L
     private boolean hasWindow; // Ob das Gehäuse ein Fenster hat oder nicht
     private int fanSlots; // Anzahl der Slots für Gehäuselüfter (im ersten Schritt nur 120mm Lüfter)
     
-    public Case(double price, String manufacturer, String model, String form, boolean hasWindow, int fanSlots) {
+    public Case(double price, String manufacturer, String model, String formMB, String formPSU, boolean hasWindow, int fanSlots) {
         super(price, manufacturer, model);
         this.type = new Type("Case");
-        this.form = new Form(form);
+        this.formMB = new Form(formMB, true);
+        this.formPSU = new Form(formPSU, false);
         this.hasWindow = hasWindow;
         this.fanSlots = fanSlots;
     }
 
-    public String getForm() {
-        return form.getForm();
+    public String getFormMB() {
+        return formMB.getForm();
+    }
+
+    public String[] possibleMBs() {
+        return formMB.possibleMotherboards();
+    }
+
+    public String getFormPSU() {
+        return formPSU.getForm();
     }
 
     public boolean hasWindow() {
