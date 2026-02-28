f46c78a10947b2040cfe28373b1fbfa267596a98
Tuluhan <GalaFreakOfficial@gmail.com>
2026-02-28 14:44:01 +0100

    style: Added empty lines

--- src/test/java/PartTest.java
+++ src/test/java/PartTest.java
@@ -19,24 +19,33 @@ public class PartTest {
     public void testCorrectCase() {
         Part korrekt = new Case(499.99, "Corsair", "Fabio Schehr", "ATX", "ATX", true, 9);
         System.out.println(korrekt);
+
         assert(korrekt.getType().equals("Case"));
         System.out.println(korrekt.getType() + " richtig erkannt");
+
         assert(korrekt.getPrice() == 499.99);
         System.out.println(korrekt.getPrice() + " richtig erkannt");
+
         assert(korrekt.getManufacturer().equals("Corsair"));
         System.out.println(korrekt.getManufacturer() + " richtig erkannt");
+
         assert(korrekt.getModel().equals("Fabio Schehr"));
         System.out.println(korrekt.getModel() + " richtig erkannt");
+        
 
         Case korrektCase = (Case) korrekt;
         assert(korrektCase.getFormMB().equals("ATX"));
         System.out.println(korrektCase.getFormMB() + " richtig erkannt");
+
         assert(korrektCase.getFormPSU().equals("ATX"));
         System.out.println(korrektCase.getFormPSU() + " richtig erkannt");
+
         assert(korrektCase.hasWindow() == true);
         System.out.println(korrektCase.hasWindow() + " richtig erkannt");
+
         assert(korrektCase.getfanSlots() == 9);
         System.out.println(korrektCase.getfanSlots() + " richtig erkannt");
+
         assert(korrektCase.possibleMBs().length == 3);
         System.out.println("Anzahl möglicher Mainboardformate: " + korrektCase.possibleMBs().length + "\n");
     }
@@ -45,22 +54,29 @@ public class PartTest {
     public void testCorrectPSU() {
         Part korrekt = new PowerSupply(199.99, "Asus Rog", "Finn Hannon", "SFX", 1000, "80 Plus Gold", true, true);
         System.out.println(korrekt);
+
         assert(korrekt.getType().equals("PSU"));
         System.out.println(korrekt.getType() + " richtig erkannt");
+
         assert(korrekt.getPrice() == 199.99);
         System.out.println(korrekt.getPrice() + " richtig erkannt");
+
         assert(korrekt.getManufacturer().equals("Asus Rog"));
         System.out.println(korrekt.getManufacturer() + " richtig erkannt");
+
         assert(korrekt.getModel().equals("Finn Hannon"));
         System.out.println(korrekt.getModel() + " richtig erkannt");
 
         PowerSupply korrektPSU = (PowerSupply) korrekt;
         assert(korrektPSU.getForm().equals("SFX"));
         System.out.println(korrektPSU.getForm() + " richtig erkannt");
+
         assert(korrektPSU.getWattage() == 1000);
         System.out.println(korrektPSU.getWattage() + " richtig erkannt");
+
         assert(korrektPSU.getEfficiencyRating().equals("80 Plus Gold"));
         System.out.println(korrektPSU.getEfficiencyRating() + " richtig erkannt");
+        
         assert(korrektPSU.isModular() == true);
         System.out.println(korrektPSU.isModular() + " richtig erkannt\n");
     }
