6f5319613d3bb9c67f59805589eb64dcec423f1e
Tuluhan <GalaFreakOfficial@gmail.com>
2026-02-28 14:33:27 +0100

    feat: dual functionality for Form
    works for Motherboard and PSU now

--- src/main/java/de/ase/pcpartpicker/parts/additional/Form.java
+++ src/main/java/de/ase/pcpartpicker/parts/additional/Form.java
@@ -1,25 +1,51 @@
 package de.ase.pcpartpicker.parts.additional;
+import de.ase.pcpartpicker.parts.additional.Type;;
 
 // Klasse die Formfaktoren validiert
 public class Form {
     
     private String form;
-    private String[] formfactors = { "E-ATX", "XL-ATX", "ATX", "Micro-ATX", "Mini-ITX"};
-
-    public Form(String form) {
-        setForm(form);
+    private boolean motherboard; // true -> Formfaktor für Mainboard, false -> Formfaktor für Netzteil
+    private String[] formfactors;
+    private String[] formfactorsMB = { "E-ATX", "XL-ATX", "ATX", "Micro-ATX", "Mini-ITX"};
+    private String[] formfactorsPSU = { "ATX", "SFX-L", "SFX", "FlexATX" };
+    
+    public Form(String form, boolean motherboard) {
+        setForm(form, motherboard);
     }
 
-    private void setForm(String form) {
+    private void setForm(String form, boolean motherboard) {
+        this.motherboard = motherboard;
+        if(motherboard) {
+            formfactors = formfactorsMB;
+        } else {
+            formfactors = formfactorsPSU;
+        }
         for(String f: formfactors) {
             if(f.equals(form)) {
                 this.form = form;
                 return;
             }
         }
-        
-        throw new IllegalArgumentException(form + " is not a valid form factor. Valid form factors are: " + String.join("\n", formfactors));
+
+
+        throw new IllegalArgumentException(form + " is not a valid form factor for a " + (motherboard ? "Motherboard" : "PSU") + ". Valid form factors are: " + String.join("\n", formfactors));
     }
+
+    public String[] possibleMotherboards() {
+        String[] possibleMBs;
+        if(!motherboard)
+            throw new IllegalStateException("This method is only available for Motherboard form factors.");
+
+        for(int i = 0; i < formfactors.length; i++) {
+            if(formfactors[i].equals(form)) {
+                possibleMBs = new String[formfactors.length - i];
+                System.arraycopy(formfactors, i, possibleMBs, 0, formfactors.length - i);
+                return possibleMBs;
+            }
+        }
+        throw new IllegalStateException("Form factor " + form + " not found.");
+    } 
 
     public String getForm() {
         return form;
