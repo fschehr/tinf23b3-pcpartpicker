ada8bb4ee9faaf7d47c211265904ec56cea193b9
Tuluhan <GalaFreakOfficial@gmail.com>
2026-03-05 17:23:34 +0100

    Ist schon im main branch
    (Ich muss noch lernen wie man mit git umgeht aaaaaaah)

--- src/main/java/de/ase/pcpartpicker/domain/CPU.java
+++ src/main/java/de/ase/pcpartpicker/domain/CPU.java
@@ -7,21 +7,24 @@ import de.ase.pcpartpicker.domain.HelperClasses.Socket;
  * Klasse für die CPU-Komponente.
  * @param id Eindeutige ID der CPU
  * @param name Name der CPU
+ * @param price Preis der CPU
  * @param manufacturer Hersteller der CPU
  * @param socket Sockeltyp der CPU (z.B. AM4, LGA1200)
  * @param speedGHz Taktfrequenz der CPU in GHz
- * @param price Preis der CPU
+ * @param hasIntegratedGraphics Gibt an, ob die CPU integrierte Grafik hat
  * @author Fabio
  */
 public class CPU extends Component {
 
     private final Socket socket;
     private final double speedGHz;
+    private final boolean hasIntegratedGraphics;
 
-    public CPU(int id, String name, double price, Manufacturer manufacturer, Socket socket, double speedGHz) {
+    public CPU(int id, String name, double price, Manufacturer manufacturer, Socket socket, double speedGHz, boolean hasIntegratedGraphics) {
         super(id, name, price, manufacturer);
         this.socket = socket;
         this.speedGHz = speedGHz;
+        this.hasIntegratedGraphics = hasIntegratedGraphics;
     }
 
     public Socket getSocket() {
@@ -30,5 +33,9 @@ public class CPU extends Component {
 
     public double getspeedGHz() {
         return speedGHz;
+    }
+
+    public boolean hasIntegratedGraphics() {
+        return hasIntegratedGraphics;
     }
 }
